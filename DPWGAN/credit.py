b6d0a3f81e2f0c37aaca2983f476aad160a45bab
TianqiCS <tq.wang@hotmail.com>
2021-04-21 22:12:17 -0600

    update

--- DPWGAN/credit.py
+++ DPWGAN/credit.py
@@ -1,14 +1,15 @@
 import collections
+import multiprocessing as mp
+
 import pandas as pd
 import numpy as np
-
 from sklearn.model_selection import train_test_split
-from sklearn.manifold import TSNE
-from time import time
 
-from sklearn.preprocessing import StandardScaler
-import matplotlib.pyplot as plt
-import seaborn as sns
+from sklearn.ensemble import RandomForestClassifier, GradientBoostingClassifier,AdaBoostClassifier,ExtraTreesClassifier
+from sklearn.naive_bayes import BernoulliNB
+from sklearn.metrics import roc_auc_score
+
+from tabulate import tabulate
 
 from models.dp_wgan_mse import DP_WGAN
 
@@ -24,32 +25,32 @@ def worker(params, x_train, y_train, x_test, y_test, input_dim, z_dim, class_rat
 
 def test(syn_data, x_test, y_test):
     x_syn, y_syn = syn_data[:, :-1], syn_data[:, -1]
-    sample_features = x_syn
-    sample_class = y_syn
 
-    scr = StandardScaler()
-    sample_features = scr.fit_transform(sample_features)
-    print(sample_features.shape, sample_class.shape)
-    model = TSNE(n_components=2, random_state=0, perplexity=30)
-
-    t0 = time()
-    embedded_data = model.fit_transform(sample_features)
-    print("TSNE done in %0.3fs." % (time() - t0))
-
-    final_data = np.concatenate((embedded_data, np.expand_dims(sample_class, axis=1)), axis=1)
-    print(final_data.shape)
-    newdf = pd.DataFrame(data=final_data, columns=["Dim1", "Dim2", "Class"])
-
-    sns.FacetGrid(newdf, hue="Class", size=6).map(plt.scatter, "Dim1", "Dim2").add_legend()
-    plt.title("Perplexity=30 with normalization")
-    plt.show()
+    # test the results on learners
+    learners = []
+    names = ['BernoulliNB', 'Random Forest', 'AdaBoostClassifier', 'ExtraTreesClassifier',
+             'GradientBoostingClassifier']
+    learners.append((BernoulliNB()))
+    learners.append((RandomForestClassifier()))
+    learners.append((AdaBoostClassifier()))
+    learners.append((ExtraTreesClassifier()))
+    learners.append((GradientBoostingClassifier()))
+    result = {}
+    for i in range(0, len(learners)):
+        learners[i].fit(x_syn, y_syn)
+        pred_probs = learners[i].predict_proba(x_test)
+        auc_score = roc_auc_score(y_test, pred_probs[:, 1])
+        result[names[i]] = auc_score
+    return result
 
 
 def main():
-    root_path = "data/"
+    # load dataset
+    root_path = "../data/"
     df = pd.read_csv(root_path + "creditcard.csv")  #total 284,807 transactions only 492 frauds
     train_df, val_df = train_test_split(df, test_size=0.05)
 
+    # balance data
     training_positive_samples = train_df.loc[train_df["Class"] == 1]
     training_negative_samples = train_df.loc[train_df["Class"] == 0].sample(n=500)
     train_df = pd.concat([training_positive_samples,training_negative_samples])
@@ -58,6 +59,7 @@ def main():
     val_negative_samples = val_df.loc[val_df["Class"] == 0].sample(n=25)
     val_df = pd.concat([val_positive_samples,val_negative_samples])
 
+    # preprocessing
     class_ratios = train_df["Class"].sort_values().groupby(train_df["Class"]).size().values/train_df.shape[0]
     print("class ratio", class_ratios)
     x_train = np.nan_to_num(train_df.drop(["Class"], axis=1).values)
@@ -71,14 +73,28 @@ def main():
     print(x_train.shape, y_train.shape)
     print(x_test.shape,y_test.shape)
 
+    # setting hyper params
     params = Hyperparams(batch_size=64, micro_batch_size=8,
                          clamp_lower=-0.01, clamp_upper=0.01,
                          clip_coeff=0.1, sigma=0.8, class_ratios=class_ratios, lr=1e-4,
                          num_epochs=10)
-    worker(params, x_train, y_train, x_test, y_test, input_dim, z_dim, class_ratios)
+
+    # multi processing and take the avg of all runs
+    pool = mp.Pool(processes=6)
+    tasks = [pool.apply_async(worker, args=(params, x_train, y_train, x_test, y_test, input_dim, z_dim, class_ratios)) for i in range(6)]
+    results = [np.array(list(p.get().values())) for p in tasks]
+    avg = np.mean(results, axis=0)
+
+    table = [['BernoulliNB', 'Random Forest', 'AdaBoostClassifier', 'ExtraTreesClassifier',
+             'GradientBoostingClassifier']]
+    table += results
+    table += [avg]
+    print("Showing results of epochs = {}, sigma = {}".format(params.num_epochs, params.sigma))
+    print(tabulate(table, headers='firstrow', floatfmt=".2f"))
 
 Hyperparams = collections.namedtuple(
     'Hyperparams',
     'batch_size micro_batch_size clamp_lower clamp_upper clip_coeff sigma class_ratios lr num_epochs')
 if __name__ == "__main__":
     main()
+
